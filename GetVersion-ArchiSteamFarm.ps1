336d688c52d1852da18ad1ed80eb83208277c665
Bruce Wayne <admin@bige0.com>
2023-06-16 17:18:49 +0800

    Authenticated requests

--- GetVersion-ArchiSteamFarm.ps1
+++ GetVersion-ArchiSteamFarm.ps1
@@ -1,5 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-$json = Invoke-RestMethod 'https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/releases/latest'
+$headers = @{
+	Authorization = "Bearer $env:GITHUB_TOKEN"
+}
+
+$json = Invoke-RestMethod -Headers $headers 'https://api.github.com/repos/JustArchiNET/ArchiSteamFarm/releases/latest'
 $verison = $json.tag_name.Trim('v')
 Write-Output $verison
--- GetVersion-ClashPremium.ps1
+++ GetVersion-ClashPremium.ps1
@@ -1,5 +1,9 @@
 $ErrorActionPreference = "Stop"
 
-$json = Invoke-RestMethod 'https://api.github.com/repos/Dreamacro/clash/releases/tags/premium'
+$headers = @{
+	Authorization = "Bearer $env:GITHUB_TOKEN"
+}
+
+$json = Invoke-RestMethod -Headers $headers 'https://api.github.com/repos/Dreamacro/clash/releases/tags/premium'
 $verison = ( -Split ($json.name))[1]
 Write-Output $verison
