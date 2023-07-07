3d19d753ac5ed35464b844ec777420bffb56dbce
Bruce Wayne <admin@bige0.com>
2023-07-07 16:17:24 +0800

    feat: Add CrystalDiskInfo

---
+++ GetVersion-CrystalDiskInfo.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = "Stop"
+
+$headers = @{
+	Authorization = "Bearer $env:GITHUB_TOKEN"
+}
+
+$content = Invoke-RestMethod -Headers $headers 'https://raw.githubusercontent.com/hiyohiyo/CrystalDiskInfo/master/stdafx.h'
+
+foreach ($line in $content -split '\n') {
+	if ($line.Contains('PRODUCT_VERSION')) {
+		$version_str = ($line.Split())[-1].TrimStart('L').Trim('"')
+		$version = $null
+		if (-not[Version]::TryParse($version_str, [ref]$version)) {
+			exit 0
+		}
+		Write-Output $version.ToString()
+		break
+	}
+}
