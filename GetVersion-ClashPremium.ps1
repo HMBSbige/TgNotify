2f3282568f4c5625ce4fb0ed01542ce342b0cffa
Bruce Wayne <admin@bige0.com>
2023-11-03 19:59:50 +0800

    Drop Clash

--- GetVersion-ClashPremium.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$headers = @{
-	Authorization = "Bearer $env:GITHUB_TOKEN"
-}
-
-$json = Invoke-RestMethod -Headers $headers 'https://api.github.com/repos/Dreamacro/clash/releases/tags/premium'
-$verison = ( -Split ($json.name))[1]
-Write-Output $verison
