016a8897a81ed43e9bc350e346b11490a12cc472
Bruce Wayne <admin@bige0.com>
2024-11-17 00:57:48 +0800

    feat: Add SafeLine

---
+++ GetVersion-SafeLine.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = "Stop"
+
+$json = Invoke-RestMethod 'https://hub.docker.com/v2/repositories/chaitin/safeline-mgt/tags?page_size=2&page=1&ordering=last_updated'
+
+foreach ($item in $json.results) {
+	$version = $null
+	if ([Version]::TryParse($item.name, [ref] $version)) {
+		Write-Output $version.ToString()
+		exit 0
+	}
+}
