f8b59210ea4b01c19692997b3c5dcd0003330877
Bruce Wayne <admin@bige0.com>
2025-08-21 11:27:46 +0800

    feat: Drop Linux-LTS

--- GetVersion-Linux-LTS.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-
-$json = Invoke-RestMethod -Headers $headers 'https://archlinux.org/packages/core/x86_64/linux-lts/json/'
-$verison = $json.pkgver
-Write-Output $verison
