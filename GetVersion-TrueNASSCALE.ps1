1c1b343d08c6333996b7997ecc90b400ffafe2e3
Bruce Wayne <admin@bige0.com>
2026-04-25 23:10:06 +0800

    fix(truenas): use trains v2 feed

--- GetVersion-TrueNASSCALE.ps1
+++ GetVersion-TrueNASSCALE.ps1
@@ -1,29 +1,24 @@
 $ErrorActionPreference = 'Stop'
 
-$trainsData = Invoke-RestMethod 'https://update.freenas.org/scale/trains.json'
+$trainsData = Invoke-RestMethod 'https://update.freenas.org/scale/trains_v2.json'
 
-$activeTrains = $trainsData.trains.PSObject.Properties | Where-Object {
-    $_.Value.description.Contains('[release]') -and !$_.Value.description.Contains('end of life')
-}
+$latestVersion = $trainsData.trains.PSObject.Properties | Where-Object {
+    $description = $_.Value.description
+    $stableProperty = $_.Value.PSObject.Properties['stable']
 
-if (-not $activeTrains) {
+    $description -notmatch '\b(BETA|RC|Nightlies)\b|\[developer only\]|end of life' -and
+    (-not $stableProperty -or $stableProperty.Value -ne $false)
+} | ForEach-Object {
+    $manifest = Invoke-RestMethod "https://update.freenas.org/scale/$($_.Name)/manifest.json"
+
+    [PSCustomObject]@{
+        Version     = [version]$manifest.version
+        VersionText = $manifest.version
+    }
+} | Sort-Object Version -Descending | Select-Object -First 1 -ExpandProperty VersionText
+
+if (-not $latestVersion) {
     exit 0
 }
 
-$activeTrainsWithVersions = $activeTrains | ForEach-Object {
-    $versionMatch = [regex]::Match($_.Value.description, '(\d+\.\d+)')
-    if (-not $versionMatch.Success) {
-        throw "Failed to parse version from: $($_.Value.description)"
-    }
-    
-    [PSCustomObject]@{
-        Name    = $_.Name
-        Version = [version]$versionMatch.Groups[1].Value
-    }
-}
-
-$train = ($activeTrainsWithVersions | Sort-Object Version -Descending | Select-Object -First 1).Name
-
-$json = Invoke-RestMethod "https://update.freenas.org/scale/$train/manifest.json"
-$verison = $json.version
-Write-Output $verison
+Write-Output $latestVersion
